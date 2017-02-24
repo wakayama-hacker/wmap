2998dd0733e02dcc9219f7068c95b8782de78390
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-02-24 23:57:26 +0900

    Enable csv2json in case of single CSV file and single menu

--- tests/lib/csv2json.js
+++ tests/lib/csv2json.js
@@ -5,17 +5,54 @@ const test     = require('./test-stream')
 
 describe('gulp-csv2json', () => {
 
-  it('should csv2json one file', done => {
+  describe('single file, single menu', () => {
 
-    const CSV  = 'aaa,bbb,ccc\n0,1,2'
-    const result = [{ 'aaa':0 },{ 'bbb':1 },{ 'ccc':2 }]
+    const CSV  =
+      'menu,title,lat,lng,content\n' +
+      '串本,橋杭岩,12.345,123.45,これは橋杭岩です\n' +
+      '串本,潮岬,23.456,132.1,これは潮岬です\n'
 
-    test(CSV)
-      .pipe(csv2json())
-      .pipe(assert.length(1))
-      .pipe(assert.first(d => {
-        expect(d.contents.toString()).to.equal(JSON.stringify(result))
-      }))
-      .pipe(assert.end(done))
+    it('should return 2 files', done => {
+      test(CSV)
+        .pipe(csv2json())
+        .pipe(assert.length(2))
+        .pipe(assert.end(done))
+    })
+
+    it('should return menu array, firstly', done => {
+      test(CSV)
+        .pipe(csv2json())
+        .pipe(assert.first(file => {
+          expect(file.contents.toString()).to.equal(JSON.stringify(['串本']))
+        }))
+        .pipe(assert.end(done))
+
+    })
+
+    it('should convert csv into json', done => {
+
+      const main = [
+        {
+          'title': '橋杭岩',
+          'lat': '12.345',
+          'lng': '123.45',
+          'content': 'これは橋杭岩です'
+        },
+        {
+          'title': '潮岬',
+          'lat': '23.456',
+          'lng': '132.1',
+          'content': 'これは潮岬です'
+        }
+      ]
+
+      test(CSV)
+        .pipe(csv2json())
+        .pipe(assert.second(file => {
+          expect(file.contents.toString()).to.equal(JSON.stringify(main))
+        }))
+        .pipe(assert.end(done))
+    })
+
   })
 })
