4e0fe01b3274ec57cf52ab00cfee4ab49a337b54
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-03-04 10:53:11 +0900

    Add openstreetmap tag

--- tags/open-street-map.tag
+++ tags/open-street-map.tag
@@ -1,24 +1,32 @@
-<open-street-map class="open-street-map">
-
-  <div
-    id={ id }
-    class={ classes }
-    data-lat={ lat }
-    data-lng={ lng }
-    data-address={ address }
-  ></div>
+<open-street-map class={ classes }>
+  <div id={ id }></div>
 
   <script type="es6">
 
-    this.id      = opts.id
-    this.classes = [
-      'map-canvas',
-      'osm-map-canvas'
-    ].concat( ( opts.classes || [] ) ).join( ' ' )
-    this.lat     = opts.lat
-    this.lng     = opts.lng
-    this.address = opts.address
+    const L = require( 'mapbox.js' )
 
+    // generate id
+    const id = 'osm-' + this._riot_id
+
+    this.id = id
+
+    // default class
+    this.classes = [
+      'map',
+      'open-street-map'
+    ].concat( ( opts.classes || [] ) ).join( ' ' )
+
+    // parse map arguments
+    const lat   = opts.lat    || 0
+    const lng   = opts.lng    || 0
+    const zoom  = opts.zoom   || 10
+    const PUBLIC_ACCESS_TOKEN = 'pk.eyJ1Ijoia2FtYXRhcnlvIiwiYSI6ImNpenVmNnJ2MTAwMDQycXBwZmloeng0ZTQifQ.5oBaHAa2R9HZl_FJjPGHSQ'
+
+    this.on( 'mount', function() {
+      L.mapbox.accessToken = PUBLIC_ACCESS_TOKEN
+      const map = L.mapbox.map( id, 'mapbox.streets' ).setView( [ lat, lng ], zoom )
+      L.marker( [ lat, lng ] ).addTo( map )
+    } )
   </script>
 
 </open-street-map>
--- tests/tags/open-street-map.js
+++ tests/tags/open-street-map.js
@@ -1,5 +1,9 @@
 describe( 'open-street-map specs', function() {
 
+  var require_prev
+  // spy
+  var spy = {}
+
   before( function() {
     // `<body />` 内の全ての要素を削除。
     // 毎回削除しないと他のテストで追加した要素が残ることに注意！
@@ -9,53 +13,57 @@ describe( 'open-street-map specs', function() {
       body.removeChild( body.firstChild )
     }
 
+    // create spy function
+    require_prev = window.require
+    window.require = function( name ) {
+      if ( name === 'mapbox.js' ) {
+        return {
+          mapbox : {
+            map : function() { return {
+              setView : function() {
+                spy['mapPosition'] = Array.prototype.slice.call( arguments )
+              }
+            } }
+          },
+          marker : function() {
+            spy['markerPosition'] = Array.prototype.slice.call( arguments )
+            return { addTo : function() {} }
+          }
+        }
+      }
+    }
+
     // `<body><open-street-map></open-street-map></body>` を作成
     var html = document.createElement( 'open-street-map' )
     body.appendChild( html )
   } )
 
-  describe( 'on container', function() {
 
-    it( 'should have class `open-street-map`', function() {
-      riot.mount( 'open-street-map', {} )
-      expect( $( 'open-street-map' ).hasClass( 'open-street-map' ) ).to.be.ok()
-    } )
+  it( 'should have class `open-street-map`', function() {
+    riot.mount( 'open-street-map', {} )
+    expect( $( '.open-street-map' ).length ).to.be( 1 )
   } )
 
-
-  describe( 'on map canvas', function() {
-
-    it( 'should have element with id', function() {
-      riot.mount( 'open-street-map', { id: 'the-id' } )
-      expect( $( 'open-street-map>div' ).attr( 'id' ) ).to.be( 'the-id' )
+  it( 'should set mapview', function() {
+    riot.mount( 'open-street-map', {
+      lat  : 12,
+      lng  : 123,
+      zoom : 12
     } )
-
-    it( 'should render default class', function() {
-      riot.mount( 'open-street-map', {} )
-      expect( $( 'open-street-map>div' ).attr( 'class' ) ).to.contain( 'map-canvas' )
-      expect( $( 'open-street-map>div' ).attr( 'class' ) ).to.contain( 'osm-map-canvas' )
-    } )
-
-    it( 'should render array classes in args', function() {
-      riot.mount( 'open-street-map', { classes: [ 'a', 'b' ] } )
-      expect( $( 'open-street-map>div' ).attr( 'class' ) ).to.contain( 'a' )
-      expect( $( 'open-street-map>div' ).attr( 'class' ) ).to.contain( 'b' )
-    } )
-
-
-    it( 'should keep lat value', function() {
-      riot.mount( 'open-street-map', { lat: 34.56 } )
-      expect( $( 'open-street-map>div' ).attr( 'data-lat' ) ).to.be( '34.56' )
-    } )
-
-    it( 'should keep lng value', function() {
-      riot.mount( 'open-street-map', { lng: 134.56 } )
-      expect( $( 'open-street-map>div' ).attr( 'data-lng' ) ).to.be( '134.56' )
-    } )
-
-    it( 'should keep address value', function() {
-      riot.mount( 'open-street-map', { address: '和歌山県白浜町x-y-z' } )
-      expect( $( 'open-street-map>div' ).attr( 'data-address' ) ).to.be( '和歌山県白浜町x-y-z' )
-    } )
+    expect( spy.mapPosition ).to.eql( [ [ 12, 123 ], 12 ] )
   } )
+
+  it( 'should set marker', function() {
+    riot.mount( 'open-street-map', {
+      lat  : 23.45,
+      lng  : 98.76,
+    } )
+    expect( spy.markerPosition ).to.eql( [ [ 23.45, 98.76 ] ] )
+  } )
+
+  after( function() {
+    // rollback overriode global require
+    window.require = require_prev
+  } )
+
 } )
